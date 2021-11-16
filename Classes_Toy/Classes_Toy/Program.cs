6a599365a5850c8b5ac4ac39922eb97859968e1d
ghern <ghern@DESKTOP-OCUVVH0>
2021-11-16 11:03:26 -0600

    Completed Part 1

---
+++ Classes_Toy/Classes_Toy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes_Toy
+{
+    public class Class
+    {
+        
+        static void Main(string[] args)
+        {
+            Toy toy1 = new Toy();
+            toy1.Manufacturer = "Hasbro";
+            toy1.Name = "Mr.Potato Head";
+            toy1.Price = 14.99;
+            toy1.AddNote("Fun for all kids");
+            toy1.AddNote("Ages 4+");
+
+            Toy toy2 = new Toy("Mattel", "Barbie", 19.99,"Super fun");
+
+            Console.WriteLine(toy1);
+            Console.WriteLine(toy2);
+        }
+    }
+}
---
+++ Classes_Toy/Classes_Toy/Toy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Toy
+{
+    public class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Notes { get; set; }
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Notes = string.Empty;
+        }
+        public Toy(string note)
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Notes = string.Empty;
+        }
+        public Toy(string manufacturer, string name, double price, string note)
+        {
+            Manufacturer = manufacturer;
+            Name = name;
+            Price = price;
+            Notes = note;
+        }
+        public void AddNote(string note)
+        {
+            if (string.IsNullOrEmpty(Notes) == true)
+            {
+                Notes = note;
+            }
+            else
+            {
+                    Notes += "\n" + note;
+            }
+        }
+        public string ReturnNote()
+        {
+            return Notes;
+        }
+        public string GetAisle()
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(1, 25);
+            string manuFirstLetter = Manufacturer;
+            manuFirstLetter = "" + manuFirstLetter.ToUpper()[0];
+            string manuLetterandNumber = $"{manuFirstLetter}{randomNumber}";
+            return manuLetterandNumber;
+        }
+        public override string ToString()
+        {  
+            return $"{Name} is made by {Manufacturer} and costs {Price.ToString("C")} and can be found on aisle {GetAisle()}. {Notes}"; ;
+        }
+    }
+}
